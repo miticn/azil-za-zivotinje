e7c7e324daea1985f4e14bea2431da1baef19994
miticn <101733519+miticn@users.noreply.github.com>
2022-06-12 21:33:31 +0200

    jsPDF + Example

--- src/views/LostPets.vue
+++ src/views/LostPets.vue
@@ -1,16 +1,25 @@
 <template>
     <div class="container">
-        
-        <div v-for="ad in allads" :key="ad.user">
+
+        <div class="oglas" v-for="ad in allads" :key="ad.user">
             <h1>{{ad.title}}</h1>{{ad.user}} 
             <h4>- {{ad.phone}} - </h4>
             <p>{{ad.description}}</p>
+            <button @click="savePDF()">PDF</button>
         </div>
     </div>
 </template>
 
+<style scoped>
+    div.oglas{
+        border: solid;
+        background-color:white;
+    }
+</style>
+
 <script>
 import allAds from "../data/ads.js"
+import jsPDF from 'jspdf'
 export default {
     name: 'LostPets',
     data(){
@@ -19,10 +28,26 @@ export default {
         }
     },
     created(){
-            if(localStorage.getItem('allAds')==null){
-                localStorage.setItem('allAds', JSON.stringify(allAds))
-            }
-            this.allads=JSON.parse(localStorage.getItem('allAds'))
+        if(localStorage.getItem('allAds')==null){
+            localStorage.setItem('allAds', JSON.stringify(allAds))
         }
+        this.allads=JSON.parse(localStorage.getItem('allAds'))
+    },
+    methods:{
+        savePDF(){
+            var doc = new jsPDF();
+
+            doc.text(20, 20, 'Hello world!');
+            doc.text(20, 30, 'This is client-side Javascript to generate a PDF.');
+
+            // Add new page
+            doc.addPage();
+            doc.text(20, 20, 'Visit CodexWorld.com');
+
+            // Save the PDF
+            doc.save('document.pdf');
+
+        }
+    }
 }
 </script>
