8ed399f97103d2c5a3a87f9347452ccaf801fb0a
Luka <lukajanke23@gmail.com>
2022-06-13 05:18:02 +0200

    Luka Language

---
+++ src/data/adPageLang.js
@@ -0,0 +1,22 @@
+const AdPageLang = {
+    en: {
+        title: 'Ad your ad',
+        name: "Pet's name: ",
+        desc: 'Description',
+        phone: 'Phone number: ',
+        add: 'Add',
+        added: 'Your Ad is successfuly added!',
+        check: 'Check it out!'
+    },
+    sr: {
+        title: 'Postavi oglas',
+        name: "Naziv ljubimca: ",
+        desc: 'Opis',
+        phone: 'Broj telefona: ',
+        add: 'Postavi',
+        added: 'Vaš oglas je uspešno postavljen!',
+        check: 'Čekirajte oglas!'
+    }
+}
+
+export default AdPageLang;
