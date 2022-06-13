2c9183ce9f7a3a5fbef4a5a6b403883a2210f050
miticn <101733519+miticn@users.noreply.github.com>
2022-06-13 06:51:37 +0200

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -19,9 +19,9 @@
                 <span class="visually-hidden">Toggle Dropdown</span>
               </a>
               <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                <li><a class="dropdown-item" href="/animals/dog/">Dogs</a></li>
+                <li><a class="dropdown-item" href="/animals/cat/">Cats</a></li>
+                <li><a class="dropdown-item" href="/animals/bird/">Birds</a></li>
               </ul>
             </div>
 
